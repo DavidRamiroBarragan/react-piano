42a578bbb2930c3240f45fc1a46b90a9c8c875cd
David Ramiro <dramirobarragan@gmail.com>
2020-12-26 12:26:20 +0100

    HOC withInstrument

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,19 +1,119 @@
-import { FunctionComponent } from "react";
-import { SoundfontProvider } from "../../adapters/Soundfont/SoundfontProvider";
+import { Component, ComponentType, FunctionComponent } from "react";
+import Soundfont, { InstrumentName, Player } from "soundfont-player";
+import { MidiValue } from "../../domain/note";
+import { AudioNodesRegistry, DEFAULT_INSTRUMENT } from "../../domain/sound";
+import { Optional } from "../../domain/types";
 import { useInstrument } from "../../state/Instrument/Context";
 import { useAudioContext } from "../AudioContextProvider/useAudioContext";
 import { Keyboard } from "./Keyboard";
 import "./styles.css";
+interface InjectedProps {
+  loading: boolean;
+  play(note: MidiValue): Promise<void>;
+  stop(note: MidiValue): Promise<void>;
+}
+
+interface ProviderProps {
+  AudioContext: AudioContextType;
+  instrument: InstrumentName;
+}
+
+interface ProviderState {
+  loading: boolean;
+  current: Optional<InstrumentName>;
+}
+
+export function withInstrument<TProps extends InjectedProps = InjectedProps>(
+  WrappedComponent: ComponentType<TProps>
+) {
+  const displayName =
+    WrappedComponent.displayName || WrappedComponent.name || "Component";
+
+  return class WithInstrument extends Component<ProviderProps, ProviderState> {
+    public static displayName = `withInstrument(${displayName})`;
+
+    public static defaultProps = {
+      instrument: DEFAULT_INSTRUMENT,
+    };
+
+    private audio: AudioContext;
+    private player: Optional<Player> = null;
+    private activeNodes: AudioNodesRegistry = {};
+
+    public state: ProviderState = {
+      loading: false,
+      current: null,
+    };
+
+    private resume = async () => {
+      return this.audio.state === "suspended"
+        ? await this.audio.resume()
+        : Promise.resolve();
+    };
+
+    private load = async (instrument: InstrumentName) => {
+      this.setState({ loading: true });
+      this.player = await Soundfont.instrument(this.audio, instrument);
+
+      this.setState({ loading: false, current: instrument });
+    };
+
+    constructor(props: ProviderProps) {
+      super(props);
+
+      const { AudioContext } = this.props;
+      this.audio = new AudioContext();
+    }
+
+    public componentDidMount() {
+      const { instrument } = this.props;
+      this.load(instrument);
+    }
+
+    public shouldComponentUpdate({ instrument }: ProviderProps) {
+      return this.state.current !== instrument;
+    }
+
+    public componentDidUpdate({ instrument: prevInstrument }: ProviderProps) {
+      const { instrument } = this.props;
+      if (instrument && instrument !== prevInstrument) this.load(instrument);
+    }
+
+    public play = async (note: MidiValue) => {
+      await this.resume();
+      if (!this.player) return;
+
+      const node = this.player.play(note.toString());
+      this.activeNodes = { ...this.activeNodes, [note]: node };
+    };
+
+    public stop = async (note: MidiValue) => {
+      await this.resume();
+      if (!this.activeNodes[note]) return;
+
+      this.activeNodes[note]!.stop();
+      this.activeNodes = { ...this.activeNodes, [note]: null };
+    };
+
+    public render() {
+      const injected = {
+        loading: this.state.loading,
+        play: this.play,
+        stop: this.stop,
+      } as InjectedProps;
+
+      return <WrappedComponent {...this.props} {...(injected as TProps)} />;
+    }
+  };
+}
+
+const WrappedKeyboard = withInstrument(Keyboard);
 
 export const KeyboardWithInstrument: FunctionComponent = () => {
   const AudioContext = useAudioContext()!;
   const { instrument } = useInstrument();
 
   return (
-    <SoundfontProvider
-      AudioContext={AudioContext}
-      instrument={instrument}
-      render={(props) => <Keyboard {...props} />}
-    />
+    <WrappedKeyboard AudioContext={AudioContext} instrument={instrument} />
   );
 };
